ee08b47964dce5ac4be9f6163d6077ea41b78955
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-02-22 07:48:24 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zbindenren/deptest
+
+go 1.17
+
+require github.com/zbindenren/depricon v0.0.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/zbindenren/depricon"
+
+func main() {
+	depricon.Depricate()
+}
